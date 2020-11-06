aa5f4cd6d1cb665e99b4b626a744bb973c6d0ce5
Josh Weinstock <josh82093@gmail.com>
2020-11-06 12:16:53 -0500

    updating data sources

--- R/data_sources.R
+++ R/data_sources.R
@@ -1,22 +1,33 @@
 get_lab_data_location = function() {
-    "/net/junglebook/michiganmedicine/HPI_4705/HPI_4705_LabResults.txt"
+    "/net/junglebook/michiganmedicine/HPI_4706/HPI_4706_LabResults.txt"
+}
+
+get_unmatched_lab_data_location = function() {
+    "/net/junglebook/michiganmedicine/HPI_4705_RandomControls/HPI_4705_unmatched_ctrls_LabResults.txt"
 }
 
 get_outcome_data_location = function() {
-    "/net/junglebook/michiganmedicine/MichiganMedicine_COVID19DATA_SIMPLIFIED_20200728.txt"
+    "/net/junglebook/michiganmedicine/MichiganMedicine_COVID19DATA_SIMPLIFIED_20200908.txt"
 }
 
 
 read_in_lab_data = function() {
-    get_lab_data_location() %>%
-        vroom::vroom(.)
+    labs = vroom::vroom(c(get_lab_data_location(), get_unmatched_lab_data_location())) # reads and bind rows   
+
+    assert_not_empty(labs)
+
+    return(labs)
 }
 
 read_in_lab_data = memoise::memoise(read_in_lab_data)
 
 read_in_outcome_data = function() {
-    get_outcome_data_location() %>%
+    outcomes = get_outcome_data_location() %>%
         vroom::vroom(.)
+
+    assert_not_empty(outcomes)
+
+    return(outcomes)
 }
 
 read_in_outcome_data = memoise::memoise(read_in_outcome_data)
