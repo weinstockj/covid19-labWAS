405edc59dab8061a90487973ba7723ca41db09db
Josh Weinstock <josh82093@gmail.com>
2021-01-14 16:49:22 -0500

    adding wholenumber utilty test

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,8 @@ assert_not_empty = function(df) {
 inverse_normalize = function(x) {
     qnorm(rank(x, na.last = "keep") / (sum(!is.na(x)) + 1))
 }
+
+#' Test If A Variable Is An Integer
+#' see ?is.integer
+#'@export
+is.wholenumber = function(x, tol = .Machine$double.eps^0.5) abs(x - round(x)) < tol
