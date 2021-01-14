d4666c06442ee9b196fd1325405e9654dd623f90
Josh Weinstock <josh82093@gmail.com>
2021-01-14 16:49:01 -0500

    adding data filtering utilities

--- R/data_sources.R
+++ R/data_sources.R
@@ -10,11 +10,40 @@ get_outcome_data_location = function() {
     "/net/junglebook/michiganmedicine/MichiganMedicine_COVID19DATA_SIMPLIFIED_20200908.txt"
 }
 
+lower_case_lab_names = function(df) {
+    assert_not_empty(df)
+    stopifnot("ResultName" %in% names(df))
+
+    df %>%
+        dplyr::mutate(ResultName = tolower(ResultName))
+}
+
+get_labs_to_exclude = function() {
+    c(
+        "Estimated GFR, Non-Black",
+        "Estimated GFR, Black"
+    )
+}
+
+exclude_labs = function(df, labs_to_exclude = get_labs_to_exclude()) {
+
+    assert_not_empty(df)
+    stopifnot(is.character(labs_to_exclude))
+
+    futile.logger::flog.info(glue::glue("excluding {length(labs_to_exclude)} labs"))
+    
+    df %>%
+        dplyr::filter(!(tolower(ResultName) %in% tolower(labs_to_exclude)))
+}
 
 read_in_lab_data = function() {
     labs = vroom::vroom(c(get_lab_data_location(), get_unmatched_lab_data_location())) # reads and bind rows   
 
     assert_not_empty(labs)
+
+    labs = lower_case_lab_names(labs)
+
+    labs = exclude_labs(labs)
 
     return(labs)
 }
